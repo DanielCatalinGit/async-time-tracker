ef8e611f37cc0eb14eb4d9af74c934b7e3ae74dc
CEF\daniel.catalin <daniel.catalin@cef.es>
2026-08-08 12:17:36 +0200

    inicializa proyecto spring boot y configura entorno local aislado

---
+++ src/main/java/com/signing/dani/asynctimetracker/AsynctimetrackerApplication.java
@@ -0,0 +1,13 @@
+package com.signing.dani.asynctimetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsynctimetrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsynctimetrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/signing/dani/asynctimetracker/AsynctimetrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.signing.dani.asynctimetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsynctimetrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
